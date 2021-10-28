41b875627a4a1a39277c4347c3d255efb5aaecb5
rooptestdev <92123921+rooptestdev@users.noreply.github.com>
2021-10-29 01:21:28 +0530

    Widgets commit

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,15 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('Hello!'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My first App'),
+        ),
+        body: Text('Some default texts'),
+      ),
     );
   }
 }
