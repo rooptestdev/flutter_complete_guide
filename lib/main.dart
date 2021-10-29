2d5ee431f40f25dcf80c277c5f35551b1313eb03
rooptestdev <92123921+rooptestdev@users.noreply.github.com>
2021-10-29 21:21:12 +0530

    Visible invisible widgets

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,10 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('Answer chosen!!');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -12,7 +16,26 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('My first App'),
         ),
-        body: Text('Some default texts'),
+        body: Column(
+          children: [
+            Text('The Question!'),
+            ElevatedButton(
+              child: Text('Answer  1'),
+              onPressed: answerQuestion,
+            ),
+            ElevatedButton(
+              child: Text('Answer 2'),
+              onPressed: () => print('Answer 2 chosen'),
+            ),
+            ElevatedButton(
+              child: Text('Answer 3'),
+              onPressed: () {
+                //...
+                print('Answer 3 chosen');
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
